22ae1f0d215609f49b55c4c7c97ee1f0cd5e42ba
ermav2008 <62751361+ermav2008@users.noreply.github.com>
2020-03-27 18:34:59 +0300

    Update Calculate.java

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,6 @@
 public class Calculate {
 	public static void main(String[] arg) {
-		System.out.println("Calculate...");
+		System.out.println("Calculate...+");
 		int first = Integer.valueOf(arg[0]);
 		int second = Integer.valueOf(arg[1]);
 		int sum = first + second;
